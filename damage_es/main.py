38a1be1603e6acf081999ef1c4e7f4b45726114d
tommydl <tommydl@stud.ntnu.no>
2022-04-12 10:59:00 +0200

    damage main
    Former-commit-id: b58e3b113f0db7e9c9e46d929149366e7fee9a3a

---
+++ damage_es/main.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+import unicodedata
+import logging
+if __name__ == '__main__':
+
+    emoji = '🥕'
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--n_iterations", type=int, default=100, help="Number of iterations to test for.")
+    parser.add_argument("--batch_size", type=int, default=1, help="Batch size.")
+    parser.add_argument("--eval_freq", type=int, default=500, help="Frequency for various saving/evaluating/logging",)
+
+    parser.add_argument("--alpha", type=float, default=0.005, metavar=0.005, help="Alpha for how much noise to add")
+
+    parser.add_argument("--img", type=str, default=emoji, metavar="🐰", help="The emoji to train on")
+    parser.add_argument("--logdir", type=str, default="logs", help="Logging folder for new model")
+    parser.add_argument("--load_model_path", type=str, default="model_999500", help="Path to pre trained model")
+
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.logdir):
+        raise Exception("Logging directory '%s' not found in base folder" % args.logdir)
+
+    args.logdir = "%s/%s-%s_%s" % (args.logdir, unicodedata.name(args.img), args.mode, time.strftime("%d-%m-%Y_%H-%M-%S"))
+    os.mkdir(args.logdir)
+
+    logging.basicConfig(filename='%s/logfile.log' % args.logdir, encoding='utf-8', level=logging.INFO)
+    argprint = "\nArguments:\n"
+    for arg, value in vars(args).items():
+        argprint += ("%s: %r\n" % (arg, value))
+    logging.info(argprint)
+
+    #dmg here
+    
