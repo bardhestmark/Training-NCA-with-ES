3ba02e49ea7b71ae308da83621168f3745bef12b
tommydl <tommydl@stud.ntnu.no>
2022-04-12 12:16:15 +0200

    damage py
    Former-commit-id: a5df8c0150d3c76c1276066c55f8387ec8adb280

---
+++ damage_ca/damage.py
@@ -0,0 +1,51 @@
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.utils import save_image
+from model import CellularAutomataModel
+from utils import load_emoji, to_rgb
+import torch.nn.functional as F
+from torch import tensor as tt
+import numpy as np
+
+class Damage():
+    def __init__(self, args):
+        self.n_iterations = args.n_iterations
+        self.batch_size = args.batch_size
+        self.dmg_freq = args.dmg_freq
+        self.alpha = args.alpha
+        self.padding = args.padding
+        self.size = args.size+2 +self.padding
+        self.logdir = args.logdir
+        self.load_model_path = args.load_model_path
+        self.target_img = load_emoji(args.img, self.size) #rgba img
+
+        p = self.padding
+        self.pad_target = F.pad(tt(self.target_img), (0, 0, p, p, p, p))
+        h, w = self.pad_target.shape[:2]
+        self.seed = np.zeros([h, w, self.n_channels], np.float64)
+        self.seed[h // 2, w // 2, 3:] = 1.0
+        self.x0 = tt(np.repeat(self.seed[None, ...], self.batch_size, 0)) #seed
+
+        t_rgb = to_rgb(self.pad_target).permute(2, 0, 1)
+        self.net = CellularAutomataModel(n_channels=16, fire_rate=0.5, hidden_channels=32)
+        save_image(t_rgb, "%s/target_image.png" % self.logdir)
+        self.writer = SummaryWriter(self.logdir)
+
+        
+    def load_model(self, path):
+        """Load a PyTorch model from path."""
+        self.net.load_state_dict(torch.load(path))
+        self.net.double()
+
+    def run(self):
+        self.load_model(self.load_model_path) # model loaded
+        x = self.x0.clone()
+        for i in range(self.n_iterations): # fully grow first
+            x_eval = self.net(x)
+            loss=self.net.loss(x_eval, self.pad_target)
+            self.writer.add_scalar("dmg/loss", loss, i)
+
+            if i % self.dmg_freq == 0: # do damage
+                x_eval 
+                    
+    
--- damage_ca/main.py
+++ damage_ca/main.py
@@ -2,6 +2,8 @@ import argparse
 import os
 import unicodedata
 import logging
+from damage import Damage
+import time
 if __name__ == '__main__':
 
     emoji = '🥕'
@@ -10,27 +12,31 @@ if __name__ == '__main__':
 
     parser.add_argument("--n_iterations", type=int, default=100, help="Number of iterations to test for.")
     parser.add_argument("--batch_size", type=int, default=1, help="Batch size.")
-    parser.add_argument("--eval_freq", type=int, default=500, help="Frequency for various saving/evaluating/logging",)
+    parser.add_argument("--dmg_freq", type=int, default=500, help="Frequency for damaging",)
 
     parser.add_argument("--alpha", type=float, default=0.005, metavar=0.005, help="Alpha for how much noise to add")
-
+    parser.add_argument("--padding", type=int, default=0, help="Padding for image")
+    
     parser.add_argument("--img", type=str, default=emoji, metavar="🐰", help="The emoji to train on")
+    parser.add_argument("--size",type=int, default=15, help="size of image")
     parser.add_argument("--logdir", type=str, default="logs", help="Logging folder for new model")
-    parser.add_argument("--load_model_path", type=str, default="model_999500", help="Path to pre trained model")
+    parser.add_argument("--load_model_path", type=str, default="models/model_999500", help="Path to pre trained model")
 
     args = parser.parse_args()
 
     if not os.path.isdir(args.logdir):
         raise Exception("Logging directory '%s' not found in base folder" % args.logdir)
 
-    args.logdir = "%s/%s-%s_%s" % (args.logdir, unicodedata.name(args.img), args.mode, time.strftime("%d-%m-%Y_%H-%M-%S"))
+    args.logdir = "%s/%s_%s" % (args.logdir, unicodedata.name(args.img), time.strftime("%d-%m-%Y_%H-%M-%S"))
     os.mkdir(args.logdir)
 
-    logging.basicConfig(filename='%s/logfile.log' % args.logdir, encoding='utf-8', level=logging.INFO)
+    print(args.logdir)
+    logging.basicConfig(handlers=[logging.FileHandler(filename=f'{args.logdir}/logfile.log', encoding='utf-8', mode='a+')], level=logging.INFO)
     argprint = "\nArguments:\n"
     for arg, value in vars(args).items():
         argprint += ("%s: %r\n" % (arg, value))
     logging.info(argprint)
 
     #dmg here
+    dmg = Damage(args)
     
