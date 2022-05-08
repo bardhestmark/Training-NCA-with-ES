deeda58cec79c1bca1b2555927292ea8123f16bf
tommydl <tommydl@stud.ntnu.no>
2022-05-08 10:33:08 +0200

    get directories from test results
    Former-commit-id: c53c94dc6f7dc61f794b194a0bbe30157e9dfca1 [formerly 55789f3ff160b399d6bf7e3b239852cfcafb94bd]
    Former-commit-id: 555a2926d864334b03f9f6cdd5c667a4dafba2c3

--- dataprocessing/main.py
+++ dataprocessing/main.py
@@ -2,14 +2,24 @@ import numpy as np
 from utils import convert_tb_data
 import tensorboard as tb
 import matplotlib.pyplot as plt
+import os
+from colorama import Fore
 plt.style.use('seaborn-whitegrid')
 
-logdir = 'interactive_CA\logs'
-eventpath = 'ADAM-RABBIT FACE_07-05-2022_14-34-42'
-path = f"{logdir}/{eventpath}"
-df = convert_tb_data(path)
+dirs = ['..\interactive_CA\logs', '..\\final_models']
+logdir = dirs[0]
+eventdirs = [d for d in os.listdir(logdir)]
 
-fig = plt.figure()
-ax = plt.axes()
-ax.plot(df['step'], df['value'])
-plt.show()
+for dir in eventdirs:
+    path = f"{logdir}/{dir}"
+
+    try:
+        df = convert_tb_data(path)
+    except ValueError as ve:
+        print(Fore.RED+f'A directory does not include a tensorboard file:{ve}')
+        Fore.WHITE
+
+    fig = plt.figure()
+    ax = plt.axes()
+    ax.plot(df['step'], df['value'])
+    plt.show()
